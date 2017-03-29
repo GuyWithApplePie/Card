ab185f016f1ae19fbefaf3165f0ef3b1aa9ce7ba
176834 <nicholas.couzelis@ucdsb.on.ca>
2017-03-29 13:13:36 -0400

    Basic checks finished.

--- src/card/Frame.java
+++ src/card/Frame.java
@@ -9,13 +9,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtInput;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtSuit;
+	private JTextField txtValue;
 
 	/**
 	 * Launch the application.
@@ -46,11 +48,41 @@ public class Frame extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnRandom = new JButton("Random Card");
+		//A random card is selected and displayed.
+		btnRandom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Variable List
+				int num = (int)(Math.random()*52); //Random number from 1-52
+				txtInput.setText("");
+
+			
+			}
+		});
 		btnRandom.setMargin(new Insets(2, 2, 2, 2));
 		btnRandom.setBounds(10, 11, 89, 23);
 		contentPane.add(btnRandom);
-		
+
 		JButton btnInput = new JButton("Input");
+		//The input value is checked then the corresponding card is displayed.
+		btnInput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Variable List
+				int num = 0;	//User input.
+				
+				try{			//Checking for integer.
+					num = Integer.parseInt(txtInput.getText());
+				} catch(NullPointerException|NumberFormatException x){
+					txtSuit.setText("Invalid Input.");
+					txtValue.setText("");
+					return;
+				}
+				
+				if(!(num<=52&&num>0)){	//Checking if input value is 0-52
+					txtSuit.setText("Input 1-52");
+					txtValue.setText("");
+				}
+			}
+		});
 		btnInput.setMargin(new Insets(2, 2, 2, 2));
 		btnInput.setBounds(50, 45, 49, 23);
 		contentPane.add(btnInput);
@@ -60,16 +92,16 @@ public class Frame extends JFrame {
 		contentPane.add(txtInput);
 		txtInput.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setBounds(109, 12, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtSuit = new JTextField();
+		txtSuit.setEditable(false);
+		txtSuit.setBounds(109, 12, 86, 20);
+		contentPane.add(txtSuit);
+		txtSuit.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setBounds(109, 46, 86, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtValue = new JTextField();
+		txtValue.setEditable(false);
+		txtValue.setBounds(109, 46, 86, 20);
+		contentPane.add(txtValue);
+		txtValue.setColumns(10);
 	}
 }
