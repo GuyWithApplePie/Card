917ff06d7956ceda70a10b29dfa7732ca02c51b0
176834 <nicholas.couzelis@ucdsb.on.ca>
2017-03-29 13:59:00 -0400

    Donerino

--- src/card/Frame.java
+++ src/card/Frame.java
@@ -12,12 +12,17 @@ import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.awt.color.*;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField txtInput;
-	private JTextField txtSuit;
 	private JTextField txtValue;
+	private JTextField txtSuit;
 
 	/**
 	 * Launch the application.
@@ -39,6 +44,7 @@ public class Frame extends JFrame {
 	 * Create the frame.
 	 */
 	public Frame() {
+		setTitle("Cards");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 219, 114);
@@ -54,9 +60,49 @@ public class Frame extends JFrame {
 				//Variable List
 				int num = (int)(Math.random()*52); //Random number from 1-52
 				txtInput.setText("");
-
-			
+			if(num<=13){
+				txtSuit.setText("Clubs");
+				txtSuit.setForeground(Color.BLACK);
+			}else if(num<=26){
+				txtSuit.setText("Spades");
+				txtSuit.setForeground(Color.BLACK);
+			}else if(num<=39){
+				txtSuit.setText("Hearts");
+				txtSuit.setForeground(Color.RED);
+			}else{
+				txtSuit.setText("Diamonds");
+				txtSuit.setForeground(Color.RED);
 			}
+
+			switch(num % 13){
+			case 1:	txtValue.setText("Ace");
+					break;
+			case 2:	txtValue.setText("Two");
+					break;
+			case 3:	txtValue.setText("Three");
+					break;
+			case 4:	txtValue.setText("Four");
+					break;
+			case 5:	txtValue.setText("Five");
+					break;
+			case 6:	txtValue.setText("Six");
+					break;
+			case 7:	txtValue.setText("Seven");
+					break;
+			case 8:	txtValue.setText("Eight");
+					break;
+			case 9:	txtValue.setText("Nine");
+					break;
+			case 10:txtValue.setText("Ten");
+					break;
+			case 11:txtValue.setText("Jack");
+					break;
+			case 12:txtValue.setText("Queen");
+					break;
+			case 13:txtValue.setText("King");
+					break;
+			}
+		}
 		});
 		btnRandom.setMargin(new Insets(2, 2, 2, 2));
 		btnRandom.setBounds(10, 11, 89, 23);
@@ -72,15 +118,59 @@ public class Frame extends JFrame {
 				try{			//Checking for integer.
 					num = Integer.parseInt(txtInput.getText());
 				} catch(NullPointerException|NumberFormatException x){
-					txtSuit.setText("Invalid Input.");
-					txtValue.setText("");
+					txtValue.setText("Invalid Input.");
+					txtSuit.setText("");
 					return;
 				}
 				
-				if(!(num<=52&&num>0)){	//Checking if input value is 0-52
-					txtSuit.setText("Input 1-52");
-					txtValue.setText("");
+				if(!(num<53&&num>0)){	//Checking if input value is 0-52
+					txtValue.setText("Input 1-52");
+					txtSuit.setText("");
+					return;
 				}
+				if(num<=13){
+					txtSuit.setText("Clubs");
+					txtSuit.setForeground(Color.BLACK);
+				}else if(num<=26){
+					txtSuit.setText("Spades");
+					txtSuit.setForeground(Color.BLACK);
+				}else if(num<=39){
+					txtSuit.setText("Hearts");
+					txtSuit.setForeground(Color.RED);
+				}else{
+					txtSuit.setText("Diamonds");
+					txtSuit.setForeground(Color.RED);
+				}
+
+				switch(num % 13){
+				case 0: txtValue.setText("King");
+						break;
+				case 1:	txtValue.setText("Ace");
+						break;
+				case 2:	txtValue.setText("Two");
+						break;
+				case 3:	txtValue.setText("Three");
+						break;
+				case 4:	txtValue.setText("Four");
+						break;
+				case 5:	txtValue.setText("Five");
+						break;
+				case 6:	txtValue.setText("Six");
+						break;
+				case 7:	txtValue.setText("Seven");
+						break;
+				case 8:	txtValue.setText("Eight");
+						break;
+				case 9:	txtValue.setText("Nine");
+						break;
+				case 10:txtValue.setText("Ten");
+						break;
+				case 11:txtValue.setText("Jack");
+						break;
+				case 12:txtValue.setText("Queen");
+						break;
+				}
+				
 			}
 		});
 		btnInput.setMargin(new Insets(2, 2, 2, 2));
@@ -92,16 +182,23 @@ public class Frame extends JFrame {
 		contentPane.add(txtInput);
 		txtInput.setColumns(10);
 		
+		txtValue = new JTextField();
+		txtValue.setHorizontalAlignment(SwingConstants.CENTER);
+		txtValue.setEditable(false);
+		txtValue.setBounds(109, 12, 86, 20);
+		contentPane.add(txtValue);
+		txtValue.setColumns(10);
+		
 		txtSuit = new JTextField();
+		txtSuit.setHorizontalAlignment(SwingConstants.CENTER);
 		txtSuit.setEditable(false);
-		txtSuit.setBounds(109, 12, 86, 20);
+		txtSuit.setBounds(109, 46, 86, 20);
 		contentPane.add(txtSuit);
 		txtSuit.setColumns(10);
 		
-		txtValue = new JTextField();
-		txtValue.setEditable(false);
-		txtValue.setBounds(109, 46, 86, 20);
-		contentPane.add(txtValue);
-		txtValue.setColumns(10);
+		JLabel lblOf = new JLabel("of");
+		lblOf.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOf.setBounds(128, 31, 46, 14);
+		contentPane.add(lblOf);
 	}
 }
